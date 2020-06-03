a545bec9bc3ef7607cce83bbf85481390836533c
navjordj <asd>
2020-06-03 13:09:13 +0200

    add params to herbivore

--- biosim/animals/herbivore.py
+++ biosim/animals/herbivore.py
@@ -6,6 +6,24 @@ from .animals import Animal
 
 
 class Herbivore(Animal):
+
+    params = {
+        "w_birth": 8.0,
+        "sigma_birth": 1.5,
+        "beta": 0.9,
+        "eta": 0.05,
+        "a_half": 40.0,
+        "phi_age": 0.6,
+        "weight_half": 10.0,
+        "mu": 0.25,
+        "gamma": 0.2,
+        "zeta": 3.5,
+        "xi": 1.2,
+        "omega": 0.4,
+        "F": 10.0,
+        "delta_phi_max": None
+    }
+    
     def __init__(self, age, weight):
         super().__init__(age, weight)
         pass
