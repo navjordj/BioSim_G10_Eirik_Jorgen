f7ef5369300f77654e3d7e3d6659f39ef53d2ac0
Eirik Høyheim <eirihoyh@nmbu.no>
2020-06-03 10:19:24 +0200

    rewrote mistake

--- biosim/animals/carnivore.py
+++ biosim/animals/carnivore.py
@@ -2,7 +2,7 @@ __author__ = 'Eirik Høyheim, Jørgen Navjord'
 __email__ = 'eirihoyh@nmbu.no ,navjordj@gmail.com'
 
 
-from animal import Animal
+from animals import Animal
 
 class Carnivore(Animal):
     def __init__(self, weight, pos):
