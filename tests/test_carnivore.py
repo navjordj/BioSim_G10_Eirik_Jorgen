a66e1f57f61d1bee78ffe819c29d7809fb34a2f1
navjordj <asd>
2020-06-03 12:18:36 +0200

    tests written for carnivore

--- tests/test_carnivore.py
+++ tests/test_carnivore.py
@@ -6,6 +6,16 @@ import pytest
 
 from biosim.animals.carnivore import Carnivore
 
-@pytest.mark.skip(reason="Not implemented yet")
+#@pytest.mark.skip(reason="Not implemented yet")
 def test_carnivore():
-    pass
+    c = Carnivore(2, 10)
+    assert c.weight == 10
+    assert c.age == 2
+
+    with pytest.raises(ValueError) as error:
+        print(error)
+        c = Carnivore(-1, 10)
+
+    with pytest.raises(ValueError) as error:
+        print(error)
+        c = Carnivore(2, -10)
