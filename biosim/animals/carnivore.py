634b9bbad45c5da95926a66c37d7f6cdea7aa47c
navjordj <asd>
2020-06-03 12:19:09 +0200

    fixed imports in carnivore/herbivore

--- biosim/animals/carnivore.py
+++ biosim/animals/carnivore.py
@@ -2,9 +2,14 @@ __author__ = 'Eirik Høyheim, Jørgen Navjord'
 __email__ = 'eirihoyh@nmbu.no ,navjordj@gmail.com'
 
 
-from biosim.animals.animals import Animal
+from .animals import Animal
 
 class Carnivore(Animal):
-    def __init__(self, weight, pos):
-        super().__init__(weight, pos)
-        pass
+    def __init__(self, weight, age):
+        super().__init__(weight, age)
+        
+
+
+if __name__ == "__main__":
+    c = Carnivore(0, 10)
+    print(c.weight)
--- biosim/animals/herbivore.py
+++ biosim/animals/herbivore.py
@@ -2,10 +2,14 @@ __author__ = 'Eirik Høyheim, Jørgen Navjord'
 __email__ = 'eirihoyh@nmbu.no ,navjordj@gmail.com'
 
 
-from biosim.animals.animals import Animal
+from animals import Animal
 
 
 class Herbivore(Animal):
     def __init__(self, weight, pos):
         super().__init__(weight, pos)
         pass
+
+
+if __name__ == "__main__":
+    h = Herbivore(0, 10)
