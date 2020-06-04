ceaf5711744b09e91337e6a88c6b5619e874f66f
Jørgen Navjord <navjordj@gmail.com>
2020-06-04 14:11:49 +0200

    pep8 refactor

--- biosim/animals/animals.py
+++ biosim/animals/animals.py
@@ -5,30 +5,33 @@ import random
 from math import exp
 from typing import Union
 
+
 def fitness_calc(phi_age: float, a: float, a_half: float, phi_weight: float, w: float, w_half: float) -> float:
 
-    def q(x: float, x_half: float, phi: float, sign: int) -> float: 
-            return 1/ (1 + exp(sign * phi*(x - x_half)))
-    
-    return q(a, a_half, phi_age, 1) * q(w, w_half, phi_weight, -1) 
+    def q(x: float, x_half: float, phi: float, sign: int) -> float:
+        return 1 / (1 + exp(sign * phi*(x - x_half)))
+
+    return q(a, a_half, phi_age, 1) * q(w, w_half, phi_weight, -1)
+
 
 params = {
-        "w_birth": 8.0,
-        "sigma_birth": 1.5,
-        "beta": 0.9,
-        "eta": 0.05,
-        "a_half": 40.0,
-        "phi_age": 0.6,
-        "weight_half": 10.0,
-        "phi_weight": 0.1,
-        "mu": 0.25,
-        "gamma": 0.2,
-        "zeta": 3.5,
-        "xi": 1.2,
-        "omega": 0.4,
-        "F": 10.0,
-        "delta_phi_max": None
-    }
+    "w_birth": 8.0,
+    "sigma_birth": 1.5,
+    "beta": 0.9,
+    "eta": 0.05,
+    "a_half": 40.0,
+    "phi_age": 0.6,
+    "weight_half": 10.0,
+    "phi_weight": 0.1,
+    "mu": 0.25,
+    "gamma": 0.2,
+    "zeta": 3.5,
+    "xi": 1.2,
+    "omega": 0.4,
+    "F": 10.0,
+    "delta_phi_max": None
+}
+
 
 class Animal:
 
@@ -37,18 +40,16 @@ class Animal:
             raise ValueError("Age must be positive")
         else:
             self._age: float = age
-        
+
         if weight <= 0:
             raise ValueError("Weight must be positive")
         else:
             self._weight: float = weight
 
-        self._params: dict =  params
+        self._params: dict = params
 
-    
-    def __str__(self): 
+    def __str__(self):
         return f'Type: {type(self)} \n Age: {self._age} \n Fitness: {self.get_fitness()}'
-        
 
     def increase_age(self):
         self._age += 1
@@ -56,18 +57,16 @@ class Animal:
 
     def death(self):
         pass
-
-
-    def give_birth(self, gamma: float, phi: float, N: float) -> Union[object, int]: # TODO update correct type
+    # TODO update correct type
+    def give_birth(self, gamma: float, phi: float, N: float) -> Union[object, int]:
         p = min(1, gamma*phi*(N-1))
         if random.random() < p:
             return type(self)(0, 3)
         else:
             return 0
-    
+
     def update_weight(self, change: float) -> None:
         self._weight += change
-
 
     def new_year(self) -> None:
         self._age += 1
@@ -78,10 +77,10 @@ class Animal:
         if self._weight < 0:
             return 0
         else:
-            self._fitness = fitness_calc(self._params["phi_age"], self._age, self._params["a_half"], self._params["phi_weight"], self._weight, self._params["weight_half"])
-        
-        return self._fitness
+            self._fitness = fitness_calc(self._params["phi_age"], self._age, self._params["a_half"],
+                                         self._params["phi_weight"], self._weight, self._params["weight_half"])
 
+        return self._fitness
 
     @property
     def age(self):
