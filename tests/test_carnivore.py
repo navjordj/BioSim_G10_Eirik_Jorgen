16b45e29887e885f42da00035c70fe63297ffabe
Jørgen Navjord <navjordj@gmail.com>
2020-06-04 13:55:19 +0200

    add types to carnivore and herbivore tests

--- tests/test_carnivore.py
+++ tests/test_carnivore.py
@@ -8,22 +8,22 @@ from biosim.animals.carnivore import Carnivore
 
 #@pytest.mark.skip(reason="Not implemented yet")
 def test_init_carnivore():
-    c = Carnivore(age=2, weight=10)
+    c: Carnivore = Carnivore(age=2, weight=10)
     assert c.weight == 10
     assert c.age == 2
 
     # Negative age should raise valuerror 
     with pytest.raises(ValueError) as error:
         print(error)
-        c = Carnivore(age=-1, weight=10)
+        c: Carnivore = Carnivore(age=-1, weight=10)
 
     # Negative weight should raise valuerror 
     with pytest.raises(ValueError) as error:
         print(error)
-        c = Carnivore(age=2, weight=-10)
+        c: Carnivore = Carnivore(age=2, weight=-10)
 
 # TODO Nødvendig å teste subclassene?
 def test_aging():
-    a = Carnivore(age=4, weight=10)
-    a.increase_age()
-    assert a.age == 5
+    c: Carnivore = Carnivore(age=4, weight=10)
+    c.increase_age()
+    assert c.age == 5
--- tests/test_herbivore.py
+++ tests/test_herbivore.py
@@ -8,20 +8,20 @@ from biosim.animals.herbivore import Herbivore
 
 # @pytest.mark.skip(reason="Not implemented yet")
 def test_init_herbivore():
-    h = Herbivore(age=2, weight=10)
+    h: Herbivore = Herbivore(age=2, weight=10)
     assert h.weight == 10
     assert h.age == 2
 
     with pytest.raises(ValueError) as error:
         print(error)
-        h = Herbivore(age=-1, weight=10)
+        h: Herbivore = Herbivore(age=-1, weight=10)
 
     with pytest.raises(ValueError) as error:
         print(error)
-        h = Herbivore(age=2, weight=-10)
+        h: Herbivore = Herbivore(age=2, weight=-10)
 
 
 def test_aging():
-    a = Herbivore(age=4, weight=10)
-    a.increase_age()
-    assert a.age == 5
+    h: Herbivore = Herbivore(age=4, weight=10)
+    h.increase_age()
+    assert h.age == 5
