ff8255b12d9e77a74d83e620a1176d29a92327e9
Jørgen Navjord <navjordj@gmail.com>
2020-06-04 14:10:56 +0200

    add fitness tests to h and c

--- tests/test_carnivore.py
+++ tests/test_carnivore.py
@@ -27,3 +27,32 @@ def test_aging():
     c: Carnivore = Carnivore(age=4, weight=10)
     c.increase_age()
     assert c.age == 5
+
+
+def test_fitness():
+
+    # Check that fitness decreases when age increases
+    c: Carnivore = Carnivore(age=2, weight=10)
+    fit_before: float = c.get_fitness()
+    c.increase_age()
+    fit_after: float = c.get_fitness()
+    assert fit_after < fit_before
+
+    # Check that fitness increases when weight increases
+    c: Carnivore = Carnivore(age=2, weight=10)
+    fit_before: float = c.get_fitness()
+    c.update_weight(10)
+    fit_after: float = c.get_fitness()
+    assert fit_after > fit_before
+
+    # Test that the fitness goes towards 0 when aging
+    c: Carnivore = Carnivore(age=2, weight=10)
+    for _ in range(1000):
+        c.increase_age()
+    assert c.get_fitness() == pytest.approx(0)
+
+    # Test that fitness goes towards 1 as weight increases
+    c: Carnivore = Carnivore(age=2, weight=10)
+    c.update_weight(1000)
+    assert c.get_fitness() == pytest.approx(1, rel=1e-4) # TODO: Find out why this fails with standard error of 1e-6
+
--- tests/test_herbivore.py
+++ tests/test_herbivore.py
@@ -25,3 +25,31 @@ def test_aging():
     h: Herbivore = Herbivore(age=4, weight=10)
     h.increase_age()
     assert h.age == 5
+
+def test_fitness():
+
+    # Check that fitness decreases when age increases
+    a: Herbivore = Herbivore(age=2, weight=10)
+    fit_before: float = a.get_fitness()
+    a.increase_age()
+    fit_after: float = a.get_fitness()
+    assert fit_after < fit_before
+
+    # Check that fitness increases when weight increases
+    a: Herbivore = Herbivore(age=2, weight=10)
+    fit_before: float = a.get_fitness()
+    a.update_weight(10)
+    fit_after: float = a.get_fitness()
+    assert fit_after > fit_before
+
+    # Test that the fitness goes towards 0 when aging
+    a: Herbivore = Herbivore(age=2, weight=10)
+    for _ in range(1000):
+        a.increase_age()
+    assert a.get_fitness() == pytest.approx(0)
+
+    # Test that fitness goes towards 1 as weight increases
+    a: Herbivore = Herbivore(age=2, weight=10)
+    a.update_weight(10000)
+    assert a.get_fitness() == pytest.approx(1)
+
