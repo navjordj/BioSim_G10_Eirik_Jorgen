fcbc5a84b8fe5ae5d876db204853140e5ba21e34
Jørgen Navjord <navjordj@gmail.com>
2020-06-04 09:47:45 +0200

    implement fitness calculation helper

--- biosim/animals/animals.py
+++ biosim/animals/animals.py
@@ -2,9 +2,16 @@ __author__ = 'Eirik Høyheim, Jørgen Navjord'
 __email__ = 'eirihoyh@nmbu.no ,navjordj@gmail.com'
 
 import random
+from math import exp
 
-def fitness_calc() -> float:
-    pass
+def fitness_calc(phi_age, a, a_half, phi_weight, w, w_half):
+
+    def q(x, x_half, phi, sign): 
+            return 1/ (1 + exp(sign * phi*(x - x_half)))
+    
+    return q(a, a_half, phi_age, 1) * q(w, w_half, phi_weight, -1) 
+
+    
 
 class Animal:
 
@@ -27,12 +34,17 @@ class Animal:
     def death(self):
         pass
 
+    def give_birth(self, gamma: float, phi: float, N: float) -> type(self):
+        p = min(1, gamma*phi*(N-1))
+        if random.random() < p:
+            return type(self)(0, 3)
+
     
-    def update_weight(self, change):
+    def update_weight(self, change: float) -> None:
         self.weight += change
 
 
-    def new_year(self):
+    def new_year(self) -> None:
         self._age += 1
 
     @property
@@ -58,3 +70,7 @@ if __name__ == "__main__":
     a = Animal(10, 3)
     print(a.weight)
     print(a.age)
+    print(a.give_birth(1, 2, 4))
+
+    print(fitness_calc(1, 2, 3, 4, 5, 6))
+
