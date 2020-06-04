5e5845f7c248ad4f3d18771255a9c34fd126e58d
Eirik Høyheim <eirihoyh@nmbu.no>
2020-06-04 17:15:35 +0200

    Made changes to super class cell
    Started to make the __init__ function, made a function were you change the amount of fodder.
    Started to make a function were the herbivore would eat, not finished yet. Started with an add animal function. Does not do a lot yet.

--- biosim/cells/cell.py
+++ biosim/cells/cell.py
@@ -1,5 +1,49 @@
-class Cell:
-    def __init__(self, pos, animals):
-        self.pos = pos
-        self.animals = animals
+__author__ = 'Eirik Høyheim, Jørgen Navjord'
+__email__ = 'eirihoyh@nmbu.no ,navjordj@gmail.com'
 
+# from biosim.animals import carnovere.Carnivore, herbivore.Herbivore
+from typing import Union
+from typing import List
+
+
+class Cell:
+
+
+
+    def __init__(self) -> None:
+        self.carnivores = []
+        self.herbivores = []
+        self.fodder = 0
+
+    def set_parameters(self, fodder: Union[float, int]) -> None:
+        if fodder >= 0:
+            self.fodder = fodder
+        else:
+            raise ValueError('Fodder must be a positive number')
+
+    # TODO must know what's inside Animals to do well
+    def eat_herbivore(self, amount: Union[float, int]) -> bool:
+        if self.herbivores == []:
+            return False
+
+        eaten = {}
+        for animal in self.herbivores:
+            if self.fodder is 0:
+                eaten[animal] = self.fodder
+            elif self.fodder > amount:
+                eaten[animal] = amount
+                self.fodder -= amount
+            elif self.fodder <= amount:
+                eaten[animal] = amount - self.fodder
+                self.fodder = 0
+
+        return True
+
+    # TODO add None
+    def add_animals(self, carnivore: List, herbivore: List) -> None:
+        if carnivore is not None:
+            for animal in carnivore:
+                self.carnivores.append(animal)
+        if herbivore is not None:
+            for animal in herbivore:
+                self.herbivores.append(animal)
