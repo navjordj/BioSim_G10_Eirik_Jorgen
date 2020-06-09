ab410385e39f39a95b3a5a8ca88ba28a7cf2b3a8
Eirik Høyheim <eirihoyh@nmbu.no>
2020-06-09 11:34:22 +0200

    commented out matplotlib

--- biosim/simulation.py
+++ biosim/simulation.py
@@ -6,7 +6,7 @@ from .animals import Herbivore, Carnivore
 from .island import Island
 
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from typing import Dict
 
 
