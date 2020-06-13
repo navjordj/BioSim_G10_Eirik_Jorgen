139b9296f1bd1e0b0dc42d0845dd777d71e38eda
Jørgen Navjord <navjordj@gmail.com>
2020-06-13 20:17:27 +0200

    migration test: Not migrate to water and prob of migrating

--- tests/test_island.py
+++ tests/test_island.py
@@ -2,6 +2,7 @@ __author__ = 'Eirik Høyheim, Jørgen Navjord'
 __email__ = 'eirihoyh@nmbu.no ,navjordj@gmail.com'
 
 import pytest
+import numpy as np
 
 from biosim.island import Island  # type: ignore
 
@@ -53,3 +54,30 @@ class Test_island:
         k = Island('WWW\nWLW\nWLW\nWWW')
         assert k.row_len is not None
         assert k.column_len is not None
+
+
+    def test_migration(self) -> None:
+        """
+        Check that migration works 
+        """
+        np.random.seed(1)
+        # Should never migrate to a water cell
+        n_migrated = 0
+        for _ in range(1000):
+            k = Island('WWW\nWLW\nWWW')
+            k.map[1][1].add_animal("Herbivore", 10, 100)
+            k.migration()
+            if k.map[1][1].n_herbivores != 1:
+                n_migrated += 1
+        assert n_migrated == 0
+
+        n_migrated = 1
+        for _ in range(1000):
+            k = Island('WWWW\nWLLW\nWLLW\nWWWW')
+            k.map[1][1].add_animal("Herbivore", age=1, weight=1000)
+            # Probability should be 1/8 to move (0.25*1)/2 (Only two squares possible to move to)
+            k.migration()
+            if k.map[1][1].n_herbivores != 1:
+                n_migrated += 1
+        p_migration = n_migrated/1000
+        assert p_migration == pytest.approx(0.125, abs=1e-2)
