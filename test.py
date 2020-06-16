19e90524c0517972868454389388ee167699d707
navjordj <asd>
2020-06-16 15:57:05 +0200

    add make_movie to test file

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from biosim.simulation import BioSim
 import numpy as np
 
+
 # TODO: fix migration. The animals are now walking more then 1 cell a year
 
 np.random.seed(1)
@@ -36,26 +37,11 @@ kart = """\
                WWWHHHHLLLLLLLWWWWWWW
                WWWWWWWWWWWWWWWWWWWWW"""
 
-kart = """\
-            WWWWWWWWWW
-            WDDDDDDDDW
-            WDDDDDDDDW
-            WDDDDDDDDW
-            WDDDDDDDDW
-            WDDDDDDDDW
-            WDDDDDDDDW
-            WDDDDDDDDW
-            WDDDDDDDDW
-            WWWWWWWWWW"""
 
 
-
-b = BioSim(seed=123, ini_pop=ini_herbs, island_map=kart)
+b = BioSim(seed=123, ini_pop=ini_herbs, island_map=kart, img_base="img", movie_format="mp4")
 b.add_population(ini_carn)
 
-# b.set_animal_parameters('Herbivore', {'mu': 1, 'omega': 0, 'gamma': 0, 'a_half': 1000, 'eta': 0})
-# b.set_animal_parameters('Carnivore', {'mu': 1, 'omega': 0, 'gamma': 0, 'F': 0, 'a_half': 1000, 'eta': 0})
-
-b.simulate(num_years=1)
-
+b.simulate(num_years=20)
+b.make_movie()
 
