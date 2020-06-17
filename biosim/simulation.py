cdac4ce06b77d014250bdbc51b605943a5f36038
Eirik Høyheim <eirihoyh@nmbu.no>
2020-06-17 17:54:08 +0200

    updated biosim and viz to be able to choose the pause speed

--- biosim/simulation.py
+++ biosim/simulation.py
@@ -20,7 +20,7 @@ class BioSim:
             self,
             seed,
             ini_pop,
-
+            plt_speed=None,
             island_map=None,
             ymax_animals=None,
             cmax_animals=None,
@@ -70,6 +70,7 @@ class BioSim:
         else:
             self.island_map = self.load_simlation(save_name)
 
+        self.plt_speed = plt_speed
         self.ymax_animals = ymax_animals
         self.cmax_animals = cmax_animals
         self.img_base = img_base
@@ -145,7 +146,7 @@ class BioSim:
             viz.update_data(self.island_map, num_herb, num_carn)
 
             if year % vis_years == 0:
-                viz.update_fig(self.island_map, num_herb, num_carn)
+                viz.update_fig(self.island_map, num_herb, num_carn, self.plt_speed)
 
             if  img_years is not None and year % img_years == 0:
                 viz.save_fig()
