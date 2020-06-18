56b904a6adb270c5c9e42be8cfb1f7e892431a32
Jørgen Navjord <navjordj@gmail.com>
2020-06-18 08:57:50 +0200

    add serialization example

---
+++ examples/serialization.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import textwrap
+import matplotlib.pyplot as plt
+
+from biosim.simulation import BioSim
+
+"""
+Compatibility check for BioSim simulations.
+
+This script shall function with biosim packages written for
+the INF200 project June 2020.
+"""
+
+__author__ = "Hans Ekkehard Plesser, NMBU"
+__email__ = "hans.ekkehard.plesser@nmbu.no"
+
+
+if __name__ == '__main__':
+    plt.ion()
+
+    geogr = """\
+               WWWWWWWWWWWWWWWWWWWWW
+               WWWWWWWWHWWWWLLLLLLLW
+               WHHHHHLLLLWWLLLLLLLWW
+               WHHHHHHHHHWWLLLLLLWWW
+               WHHHHHLLLLLLLLLLLLWWW
+               WHHHHHLLLDDLLLHLLLWWW
+               WHHLLLLLDDDLLLHHHHWWW
+               WWHHHHLLLDDLLLHWWWWWW
+               WHHHLLLLLDDLLLLLLLWWW
+               WHHHHLLLLDDLLLLWWWWWW
+               WWHHHHLLLLLLLLWWWWWWW
+               WWWHHHHLLLLLLLWWWWWWW
+               WWWWWWWWWWWWWWWWWWWWW"""
+    geogr = textwrap.dedent(geogr)
+
+    ini_herbs = [{'loc': (10, 10),
+                  'pop': [{'species': 'Herbivore',
+                           'age': 5,
+                           'weight': 20}
+                          for _ in range(150)]}]
+    ini_carns = [{'loc': (10, 10),
+                  'pop': [{'species': 'Carnivore',
+                           'age': 5,
+                           'weight': 20}
+                          for _ in range(40)]}]
+
+    sim = BioSim(island_map=geogr, ini_pop=ini_herbs,
+                 seed=123456, data_name="data")
+
+    sim.add_population(ini_carns)
+
+    sim.simulate(num_years=5, vis_years=1, img_years=2000)
+    # A file named data.csv should appear containing the state of the sim for every year
+
+    sim.simulate(num_years=5, vis_years=1)
+    # Data should get appended to the file with correct year
